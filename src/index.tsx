95fdd125a79558bb1dbb5c76c54a7afef029fdff
Pavlo Vasylkivskyi <pavlo.vasylkivskyi@walkme.com>
2022-08-22 23:44:05 +0300

    test1

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import App from './containers/app/App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+// eslint-disable-next-line no-console
+console.log('test1');
 root.render(
   <React.StrictMode>
     <App />
